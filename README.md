5c4856a75346a05168202ffdc4b734655f374dd8
William Wareing <wwareing11@gmail.com>
2022-09-16 18:29:20 -0700

    The best I can do before I get tutoring on Sunday, and sadly my grade will be upsetting

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# William-Wareing-03
-Challenge 3 of bootcamp
+# <Password Generator>
+
+## This webpage is meant to create a randomized password between 8 and 128 characters, consisting of lowercase and upercase letters, numbers, and/or special characters depending on the input entered into the prompt boxes.
+
+## Installation
+    N/A
+
+## Usage
+    The webpage barely has any function thus far. When the button is clicked, a prompt appears asking for the first criteria with the between 8 and 128 litmit. If the number entered is not within that margin, a prompt will appear that says it must be. However, that will not stop the function from going on to the next question. All of the question prompts pop up, but the information has zero consequence when input into the text box. I have failed in this assignment, but I am going to get tutoring and figure out what I'm missing. 
+
+## Credits
+    N/A
+
+## License
+    Refer to the license in the repository. 
--- script.js
+++ script.js
@@ -1,14 +1,39 @@
+
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  const password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", charactersPrompt);
+
+function charactersPrompt() {
+  var number = prompt("Enter the number of characters you would like your password to be");
+    if (number < 9 ) {
+      alert("The password must be more than 8 characters")
+      };
+    if (number > 127) {
+      alert("The password must be less than 128 characters")
+      };
+    if (8<number<128) {
+      prompt("Include lowercase letters? Type 'yes' or 'no'");
+    };
+      prompt("Include uppercase letters? Type 'yes' or 'no'");
+      prompt("Include numbers? Type 'yes' or 'no'");
+      prompt("Include special characters? Type 'yes' or 'no'");
+};
+//let number = prompt("Enter the number of characters you would like your password to be");
+  
+
+
+
+//numericPrompt ();
