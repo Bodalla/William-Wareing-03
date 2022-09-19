cdb27a5023a621d30712dbfe6159d261fbb2df80
William Wareing <wwareing11@gmail.com>
2022-09-18 19:01:39 -0700

    tutoring session 1

--- script.js
+++ script.js
@@ -3,37 +3,87 @@
 const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  const password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword(password) {
+//   const password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
-
+//["a", "b", ]
+var lowerCaseArray="abcdefghjklimnopqrstuvwxyz".split('')
+var upperCaseArray="abcdefghjklimnopqrstuvwxyz".toUpperCase().split('')
+var numberArray="1234567890".split('')
+var specialArray ="~!@#$%^&*".split('')
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", charactersPrompt);
+generateBtn.addEventListener("click", 
+getNumber
+);
 
-function charactersPrompt() {
+function getNumber() {
   var number = prompt("Enter the number of characters you would like your password to be");
     if (number < 9 ) {
-      alert("The password must be more than 8 characters")
-      };
-    if (number > 127) {
-      alert("The password must be less than 128 characters")
-      };
-    if (8<number<128) {
-      prompt("Include lowercase letters? Type 'yes' or 'no'");
-    };
-      prompt("Include uppercase letters? Type 'yes' or 'no'");
-      prompt("Include numbers? Type 'yes' or 'no'");
-      prompt("Include special characters? Type 'yes' or 'no'");
-};
-//let number = prompt("Enter the number of characters you would like your password to be");
+      alert("The password must be more than 8 characters");
+      getNumber()
+      }
+
+     if (number > 127) {
+      alert("The password must be less than 128 characters");
+      getNumber()
+      }
+
+    // return number  
+    selectCharacter()
+    // generatePassword()
+    // writePassword()
+}
+    //generatePassword()
+
+  
+  //let number = prompt("Enter the number of characters you would like your password to be");
+  
+  var characterPool =[];
+
+  function selectCharacter() {
+    // for(var i=0; i<lowerArray.length; i++){characterPool.push(lowerCaseArray[i])}
+    
+    var answer1 = prompt("Include lowercase letters? Type 'yes' or 'no'");
+    if (answer1 ==="yes") {characterPool = [...lowerCaseArray]};
+
+    var answer2 = prompt("Include uppercase letters? Type 'yes' or 'no'");
+    if (answer2 ==="yes") {characterPool = [...upperCaseArray]};
+
+    var answer3 = prompt("Include numbers? Type 'yes' or 'no'");
+    if (answer3 ==="yes") {characterPool = [...numberArray]}
+
+    var answer4 = prompt("Include special characters? Type 'yes' or 'no'");
+    if (answer4 ==="yes") {characterPool = [...specialArray]}
+
+    
+    generatePassword(characterPool)
+    // prompt("Include uppercase letters? Type 'yes' or 'no'");
+    // prompt("Include numbers? Type 'yes' or 'no'");
+    // prompt("Include special characters? Type 'yes' or 'no'");
+  };
+  
   
 
+  function generatePassword(pool){
+    var password =[]
+    var number = getNumber()
+    var pool = selectCharacter()
+    for (var i=0; i<number; i++){
+      var index = Math.floor( Math.random() * (pool.length))
+      var character = pool[index];
+      password.push(character)
+    }
+    
+    const password1 = password.join('');
+  var passwordText = document.querySelector("#password");
 
+  passwordText.value = password1;
+  }
 
 //numericPrompt ();
